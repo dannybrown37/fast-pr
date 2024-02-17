4037767358d85ac614ae892e6a9fea32fb17bfcf
Danny Brown <dannybrown37@gmail.com>
2024-02-17 04:57:19 -0500

    chore: additional cleanup of terminal output

--- src/fast-pr.sh
+++ src/fast-pr.sh
@@ -7,7 +7,7 @@
 pr() {
 
     if git remote -v | grep -q "bitbucket"; then
-        repo_home="bitbucket"
+        repo_host="bitbucket"
         if [[ -z $BITBUCKET_TOKEN ]]; then
             echo "Error: You must set your BITBUCKET_TOKEN in the environment:"
             echo "export BITBUCKET_TOKEN=abcd"
@@ -16,25 +16,22 @@ pr() {
             return
         fi
     elif git remote -v | grep -q "github"; then
-        repo_home="github"
+        repo_host="github"
         if [[ -z $GITHUB_TOKEN ]]; then
             echo "Error: You must set your GITHUB_TOKEN in the environment"
+            echo "export GITHUB_TOKEN=efgh"
             return
         fi
     else
-        echo "Error: Repo's remote URL is not supported. Add it or stick with GitHub or Bitbucket."
+        echo "Error: Repo's remote URL is not yet supported. Add it or stick with GitHub or Bitbucket."
         git remote -v
         return
     fi
 
-    # Get default branch
     default_branch=$(git remote show origin | grep 'HEAD branch' | awk '{print $NF}')
-
-    # Get current branch
     current_branch=$(git branch | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-    pull_request_title="$current_branch -> $default_branch"
 
-    # Get commit messages for each line in branch
+    # Get commit hashes and messages for current branch, construct PR description
     non_current_branches=$(git for-each-ref --format='%(refname)' refs/heads/ | grep -v "refs/heads/$current_branch")
     commit_messages=$(git log $current_branch --oneline --not $non_current_branches)
     readarray -t commit_messages <<< "$commit_messages"
@@ -47,7 +44,9 @@ pr() {
 
     # Create PR content
 
-    if [ $repo_home = "bitbucket" ]; then
+    pull_request_title="$current_branch -> $default_branch"
+
+    if [ $repo_host = "bitbucket" ]; then
 
         if [[ -z $BITBUCKET_BASE_URL ]]; then
             # Personal/cloud Bitbucket
@@ -88,7 +87,7 @@ pr() {
         data_type_header="Content-Type: application/json"
         token=$BITBUCKET_TOKEN
 
-    elif [ $repo_home = "github" ]; then
+    elif [ $repo_host = "github" ]; then
 
         json_content="{
             \"title\": \"$pull_request_title\",
@@ -116,12 +115,12 @@ pr() {
     )
     rm -f temp_pr.json
 
-    if [ $repo_home = "bitbucket" ]; then
+    if [ $repo_host = "bitbucket" ]; then
 
         # In Bitbucket, an existing PR will simply be updated and not error out
         pr_url=$(echo "$response" | jq -r '.links.html.href')
 
-    elif [ $repo_home = "github" ]; then
+    elif [ $repo_host = "github" ]; then
 
         # In GitHub, an existing PR will be rejected with an error.
         # Just update the description with a follow-up patch request.
@@ -129,7 +128,7 @@ pr() {
         if [[ $error_message =~ ^A\ pull\ request\ already\ exists\ for\ ([^:]+):([^\.]+)\. ]]; then
             user_name="${BASH_REMATCH[1]}"
             branch_name="${BASH_REMATCH[2]}"
-            echo "This pull request already exists!"
+
             json_content="{
                 \"body\": \"$pr_description\"
             }"
@@ -137,20 +136,31 @@ pr() {
 
             pr_url="https://github.com/$user_name/$repo_name/pull/$branch_name/"
 
-            curl -X PATCH \
-                -H "Authorization: Bearer $token" \
-                -H "$data_type_header" \
-                -d @temp_patch.json \
-                "$pr_url"
+            # the web UI URL with branch name will redirect to a URL with the
+            # pull-request number, which is what we need for the API URL
+            redirected_url=$(curl -Ls -o /dev/null -w %{url_effective} $pr_url)
+            pull_number=$(basename $redirected_url)
+
+            api_url="${url}/${pull_number}.patch"
+
+            response=$(
+                curl -X PATCH \
+                    -H "Authorization: Bearer $token" \
+                    -H "$data_type_header" \
+                    -d @temp_patch.json \
+                    -s \
+                    "$api_url"
+            )
 
             rm -f temp_patch.json
+            echo "This pull request already exists, but the description has been updated."
         else
             echo "PR opened successfully!"
             pr_url=$(echo "$response" | jq -r '.html_url')
         fi
     fi
 
-    echo "Opening web browser to PR URL: $pr_url"
+    echo -e "\nOpening web browser to PR URL:\n$pr_url"
 
     # get open browser command
     case $(uname -s) in
